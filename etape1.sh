535f3c750beb9bf22bf52fe61dd26a9b0c4d64ae
Nitozo <gaylorcandel@gmail.com>
2025-06-13 15:17:36 +0200

    Modification script ajout de log (Etape1.3)

--- etape1.sh
+++ etape1.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-find "$1" -type f -exec chmod a+r {} +
+CHEMIN="$1"
+LOG="rights.log"
+
+# Parcourt tous les fichiers
+find "$CHEMIN" -type f | while read -r fichier; do
+    droits_avant=$(stat -c "%a" "$fichier")
+
+    # Vérifie si "others" n'ont pas le droit de lecture
+    if ! [ -r "$fichier" ]; then
+        chmod a+r "$fichier"
+        droits_apres=$(stat -c "%a" "$fichier")
+        echo "$(date '+%Y-%m-%d %H:%M:%S') $fichier $droits_avant -> $droits_apres" >> "$LOG"
+    fi
+done
